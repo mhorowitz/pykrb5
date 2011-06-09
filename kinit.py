e7425649203e96d3b70aa1823ec9da6052e2c3c4
Marc Horowitz <marc@nerd-militia.org>
2011-06-09 15:29:16 -0400

    make the client name optional

--- kinit.py
+++ kinit.py
@@ -4,6 +4,5 @@ import krb5.client
 
 if __name__ == '__main__':
     client = krb5.client.Client()
-    session = client.get_pw_session(krb5.client.terminal_prompter, sys.argv[1])
-
-    print "Success."
+    name = sys.argv[1] if len(sys.argv) > 1 else None
+    session = client.get_pw_session(krb5.client.terminal_prompter, name)
